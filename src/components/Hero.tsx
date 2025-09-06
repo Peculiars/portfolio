02d24f8b1be6606f1ba29eb2c4de60b3f5355979
Peculiars <olaitandamilare230@gmail.com>
2025-09-06 14:43:39 +0100

    refactor: Update styles for improved visual consistency across components

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,8 +13,8 @@ const Hero = () => {
             <Spotlight className='left-full top-10 h-[80vh] w-[50vw]' fill='purple'/>
             <Spotlight className='left-80 top-28 h-[80vh] w-[50vw] ' fill='blue'/>
         </div>
-        <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 dark:bg-grid-black/[0.2]">
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"/>
+        <div className="absolute top-0 left-0 flex h-screen w-full items-center justify-center bg-black-100 bg-grid-black/[0.2]">
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] bg-black-100"/>
         </div>
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col justify-center items-center'>
--- src/components/ui/InfiniteMovingCards.tsx
+++ src/components/ui/InfiniteMovingCards.tsx
@@ -87,13 +87,13 @@ export const InfiniteMovingCards = ({
       >
         {items.map((item, idx) => (
           <li
-            className="relative w-[90vw] max-w-full shrink-0 rounded-2xl border border-b-0 p-5 md:p-10 border-zinc-200 md:w-[60vw] dark:border-zinc-800" 
+            className="relative w-[90vw] max-w-full shrink-0 rounded-2xl border border-b-0 p-5 md:p-10 md:w-[60vw] border-zinc-800" 
             style={{ background: "rgb(4,7,29)", backgroundColor:   "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)", }}
             key={idx}
           >
             <blockquote>
               <div aria-hidden="true" className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"></div>
-              <span className="relative z-20 text-sm md:text-lg leading-[1.6] font-normal text-white dark:text-gray-100">
+              <span className="relative z-20 text-sm md:text-lg leading-[1.6] font-normal text-gray-100">
                 {item.quote}
               </span>
               <div className="relative z-20 mt-6 flex flex-row items-center">
@@ -102,10 +102,10 @@ export const InfiniteMovingCards = ({
                         <img src={item.img} alt={item.name} className="w-16 h-16 rounded-full"/>
                     </div>
                     <div className="flex flex-col gap-1">
-                        <span className="text-xl font-bold leading-[1.6] text-white dark:text-gray-400">
+                        <span className="text-xl font-bold leading-[1.6] text-gray-400">
                           {item.name}
                         </span>
-                        <span className="text-sm leading-[1.6] font-normal text-white-200 dark:text-gray-400">
+                        <span className="text-sm leading-[1.6] font-normal 0 text-gray-400">
                           {item.title}
                         </span>
                     </div>
